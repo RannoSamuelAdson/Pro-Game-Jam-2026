a1b5f1e69868f1659d896f460507df26bef52809
Nyazy <nyazydev@gmail.com>
2026-01-25 13:10:43 +0200

    Spawn piece logic improved, now multiple piceces are spawn

--- Assets/Scripts/PieceSpawner.cs
+++ Assets/Scripts/PieceSpawner.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PieceSpawner : MonoBehaviour
 {
     public GameObject piecePrefab;
     public GameObject spawnablecanvas;
+    public List<Sprite> spriteList;
+    
 
     void Start()
     {
-        SpriteRenderer bgRenderer = spawnablecanvas.GetComponent<SpriteRenderer>();
+        spawnPieces();
+    }
 
+
+    private void spawnPieces(){
+
+        foreach (Sprite s in spriteList){
+        SpriteRenderer bgRenderer = spawnablecanvas.GetComponent<SpriteRenderer>();
         float randomX = Random.Range(bgRenderer.bounds.min.x, bgRenderer.bounds.max.x);
         float randomY = Random.Range(bgRenderer.bounds.min.y, bgRenderer.bounds.max.y);
-        float z = spawnablecanvas.transform.position.z - 1f;
-        Instantiate(piecePrefab, new Vector3(randomX, randomY, z), Quaternion.identity);
+        Instantiate(piecePrefab, new Vector3(randomX, randomY, 0), Quaternion.identity);
+        }
+         
     }
 }
